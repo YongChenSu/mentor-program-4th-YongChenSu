c604e22e948bd12eb52aa48e2c1e8e58d69b6dbb
YongChenSu <yongchensu0@gmail.com>
2020-06-24 16:16:15 +0800

    Modify week2_homework3

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -7,3 +7,12 @@ function reverse(str) {
 }
 
 reverse('hello');
+
+// Method2：使用內建函式
+function reverseStr(str) {
+  let result = ''
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i]
+  }
+  console.log(result)
+}
