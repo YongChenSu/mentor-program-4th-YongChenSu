55b4446482a88ecf1d9320c3a1d12cb5294bd280
YongChenSu <yongchensu0@gmail.com>
2020-06-24 16:11:39 +0800

    Finish week2_homework5

--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,24 @@
-function join(arr, concatStr) {
-  
+function join(arr, separator) {
+  let temp = ''
+  if (arr.length === 1) {
+    temp = arr[0] + separator
+  } else {
+    for (let i = 0; i < arr.length; i++) {
+      if (i === arr.length - 1) {
+        temp += arr[i]
+      } else {
+        temp += arr[i] + separator
+      }
+    }
+  }
+  return temp
 }
-
 function repeat(str, times) {
-  
+  let result = ''
+  for (let i = 1; i <= times; i++) {
+    result += str
+  }
+  return result
 }
 
 console.log(join(['a'], '!'));
