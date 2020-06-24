d25213998827c981e82e753aff11295d744b396d
YongChenSu <yongchensu0@gmail.com>
2020-06-24 16:15:49 +0800

    Modify week2_homework2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -15,12 +15,3 @@ function capitalize(str) {
 }
 
 console.log(capitalize('hello'));
-
-// Method2：使用內建函式
-function reverseStr(str) {
-  let result = ''
-  for (let i = str.length - 1; i >= 0; i--) {
-    result += str[i]
-  }
-  console.log(result)
-}
