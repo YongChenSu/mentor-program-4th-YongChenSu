22b7d0733aa6256b297fbd6aba94600c54da7b1a
YongChenSu <yongchensu0@gmail.com>
2020-07-12 21:22:08 +0800

    modify week4_homework1

--- homeworks/week4/hw1.js
+++ homeworks/week4/hw1.js
@@ -2,9 +2,9 @@
 const request = require('request')
 const BASE_URL = 'https://lidemy-book-store.herokuapp.com/books'
 
-request(BASE_URL, (error, response, body) => {
+request(`${BASE_URL}?_limit=10`, (error, response, body) => {
   const BODY = JSON.parse(body)
-  for (let i = 0; i < 10; i + 1) {
-    console.log(BODY[i].id, BODY[i].name)
+  for (let i = 0; i < BODY.length; i += 1) {
+    console.log(`${BODY[i].id} ${BODY[i].name}`)
   }
 })
