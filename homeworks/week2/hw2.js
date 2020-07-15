db08a62cb7dc7999fff12bff1d4116898122bf02
MSI\USER <yongchensu0@gmail.com>
2020-07-15 15:48:35 +0800

    修改 week2_hw2 變數名稱

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,17 +1,16 @@
+/* eslint-disable */
 // Method1：盡量不使用內建函式
 function capitalize(str) {
-  let FirstLetter = ''
+  let firstLetter = ''
   let remainingStr = ''
-  if (str.charCodeAt(0) >= 97 &&
-    str.charCodeAt(0) <= 122
-  ) {
-    FirstLetter = String.fromCharCode(str.charCodeAt(0) - 32)
+  if (str.charCodeAt(0) >= 97 && str.charCodeAt(0) <= 122) {
+    firstLetter = String.fromCharCode(str.charCodeAt(0) - 32)
     for (let i = 1; i < str.length; i++) {
       remainingStr += str[i]
     }
-    return FirstLetter + remainingStr
+    return firstLetter + remainingStr
   }
   return str
 }
 
-console.log(capitalize('hello'));
+console.log(capitalize('hello'))
